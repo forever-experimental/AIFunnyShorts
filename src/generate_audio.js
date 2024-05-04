ef024bc9a2237acf89345572a57a024b7b705d39
renox <planetrenox@protonmail.com>
2024-05-03 22:25:12 -0700

    audio v1

--- src/generate_audio.js
+++ src/generate_audio.js
@@ -1,32 +1,33 @@
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'xi-api-key': $('#input-elvnlabs').value
-    },
-    body: JSON.stringify({
-        text: $('#textarea-tts').value,
-        model_id: 'eleven_multilingual_v2',
-        voice_settings: {
-            stability: 0.3,
-            similarity_boost: 0.5,
-            style: 0,
-            use_speaker_boost: true
-        }
-    })
-};
-
-
 $('#button-speech').onclick = () => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'xi-api-key': $('#input-elvnlabs').value
+        },
+        body: JSON.stringify({
+            text: $('#textarea-tts').value,
+            model_id: 'eleven_multilingual_v2',
+            voice_settings: {
+                stability: 0.3,
+                similarity_boost: 0.5,
+                style: 0,
+                use_speaker_boost: true
+            }
+        })
+    };
     CuteModal.show('Audio generation in progress...');
     try {
+        // Clear the existing audio source if any
+        const audio = $('#audio');
+        audio.src = '';
+
         fetch('https://api.elevenlabs.io/v1/text-to-speech/XB0fDUnXU5powFXDhCwa', options)
             .then(response => response.blob()) // Get the response as a blob
             .then(blob => {
                 const url = URL.createObjectURL(blob);
-                const audio = $('#audio');
                 audio.src = url;
-                audio.load();
+                audio.load(); // Load the new audio source into the <audio> element
             })
             .catch(err => console.error(err));
     } catch (err) {
