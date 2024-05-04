d53c7c1bfdccd27d9752d1a98a3fd2792d9382aa
renox <planetrenox@protonmail.com>
2024-05-03 22:49:58 -0700

    last edit before sleep

--- src/generate_audio.js
+++ src/generate_audio.js
@@ -1,3 +1,4 @@
+// https://elevenlabs.io/docs/api-reference/text-to-speech
 $('#button-speech').onclick = () => {
     const options = {
         method: 'POST',
@@ -7,35 +8,29 @@ $('#button-speech').onclick = () => {
         },
         body: JSON.stringify({
             text: $('#textarea-tts').value,
-            model_id: 'eleven_multilingual_v2',
+            model_id: 'eleven_multilingual_v1', // https://help.elevenlabs.io/hc/en-us/articles/21811236079505-How-do-I-find-the-model-ID
             voice_settings: {
                 stability: 0.3,
-                similarity_boost: 0.5,
-                style: 0,
+                similarity_boost: 0.0,
+                style: 0.0,
                 use_speaker_boost: true
             }
         })
     };
     CuteModal.show('Audio generation in progress...');
     try {
-        // Clear the existing audio source if any
-        const audio = $('#audio');
-        audio.src = '';
-
-        fetch('https://api.elevenlabs.io/v1/text-to-speech/XB0fDUnXU5powFXDhCwa', options)
-            .then(response => response.blob()) // Get the response as a blob
+        $('#audio').src = '';
+        fetch('https://api.elevenlabs.io/v1/text-to-speech/jsCqWAovK2LkecY7zXl4', options) // https://api.elevenlabs.io/v1/voices
+            .then(response => response.blob())
             .then(blob => {
                 const url = URL.createObjectURL(blob);
-                audio.src = url;
-                audio.load(); // Load the new audio source into the <audio> element
+                $('#audio').src = url;
+                $('#audio').load();
             })
             .catch(err => console.error(err));
     } catch (err) {
         console.error(err);
     }
-
     console.log('done...');
     CuteModal.hide();
 }
-
-
